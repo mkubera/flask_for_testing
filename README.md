22df78cb33fd66e2095a6c6696ca1291bea58173
mkubera <rainteller@tutanota.de>
2020-10-14 17:03:50 +0200

    Readme + sum_aod test

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # flask_for_testing
- 
+
+## Venv
+
+In the root of the project, run `. venv/bin/activate` (UNIX) or `venv\Scripts\activate` (Win).
+
+
+## Set env vars
+
+In the root of the project, set env vars to the following values: (UNIX: use `export`; Win: use `set`)
+	- FLASK_APP=flaskr
+	- FLASK_ENV=development
+
+
+## Initiate DB
+
+In the root of the project, run `flask init-db`.
+
+
+## Run
+
+Run the app using `flask run`.
+
+
+## Test
+
+In the root of the project, run `python -m pytest tests/`. Depending on your OS, you may have to use `python3` command instead.
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,4 +1,4 @@
-from flaskr import create_app
+from flaskr import create_app, sum_aod
 from flask import json
 
 def test_config():
@@ -7,7 +7,7 @@ def test_config():
 
 def test_root(client):
 	resp = client.get('/')
-	
+
 	assert resp.data == b"Hi, I'm the ROOT of the app"
 
 def test_api_get_strategists(client):
@@ -17,3 +17,8 @@ def test_api_get_strategists(client):
 	assert data['data'][0] == {"id": 1, "name": "Hannibal", "aod": 69}
 	assert data['aod_total'] == 177
 	
+def test_sum_aod():
+	strategists = [{'aod': 1},{'aod': 2},{'aod': 3}]
+	total = sum_aod(strategists)
+
+	assert total == 6
